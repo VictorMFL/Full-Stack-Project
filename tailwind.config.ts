b1c522ccb9d4ea23d339d756e65e4f76f9eb3a25
VictorMFL <vm859222@gmail.com>
2023-11-25 16:59:39 -0300

    chore: add colors in theme and component flex

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fff",
+        text: "#FAFAFA",
+        secondary: "##F5F5F5",
+        secondary2: "#DB4444",
+        black: "#000",
+        button01: "#00FF66"
+      },
+    },
+  },
   plugins: [],
-}
-export default config
+};
+export default config;
